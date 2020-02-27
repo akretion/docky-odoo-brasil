2d7d65196c7c826d4b36af222b75e864c47b3c31
Renato Lima <renato.lima@akretion.com>
2020-02-27 11:26:21 -0300

    Atualização do template

--- odoo/setup.py
+++ odoo/setup.py
@@ -6,11 +6,11 @@ with open('VERSION') as fd:
 setup(
     name="odoo-brasil",
     version=version,
-    description="project description",
+    description="Default Akretion Docky Template",
     license='GNU Affero General Public License v3 or later (AGPLv3+)',
-    author="Author...",
-    author_email="email...",
-    url="url...",
+    author="Renato Lima",
+    author_email="renato.lima@akretion.com.br",
+    url="akretion.com",
     packages=['songs'] + ['songs.%s' % p for p in find_packages('./songs')],
     include_package_data=True,
     classifiers=[
@@ -19,8 +19,8 @@ setup(
         'License :: OSI Approved :: '
         'GNU Affero General Public License v3 or later (AGPLv3+)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
 )
